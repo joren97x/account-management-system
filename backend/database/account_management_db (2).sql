b21bfeb61c05d4ab76f9039af74057f25c2736d7
joren97x <babamyloves2002@gmail.com>
2023-12-08 14:58:25 +0630

    db

--- backend/database/account_management_db (2).sql
+++ backend/database/account_management_db (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2023 at 02:29 PM
+-- Generation Time: Dec 08, 2023 at 09:26 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -45,13 +45,31 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_all_announcements` ()   BEGI
   SELECT * FROM announcements;
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_student` (IN `_id` INT)   BEGIN
+  SELECT * FROM users WHERE id = _id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_student_by_email` (IN `_email` VARCHAR(255))   BEGIN
+  SELECT * FROM users WHERE email = _email;
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insert_student` (IN `_first_name` VARCHAR(255), IN `_last_name` VARCHAR(255), IN `_email` VARCHAR(255), IN `_department` VARCHAR(255), IN `_password` VARCHAR(255), IN `_role` VARCHAR(255))  SQL SECURITY INVOKER BEGIN
-    INSERT INTO users(first_name, last_name, email, department, password, role)
+
+  Declare doesUserExists int;
+    set doesUserExists = 0;
+
+    SELECT count(*) into doesUserExists from users where email = _email;
+  
+   if doesUserExists = 0 then  
+     INSERT INTO users(first_name, last_name, email, department, password, role)
       VALUES(_first_name, _last_name, _email, _department, _password, _role);
+   end if;
+
+   
   END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_signInStudent` (IN `_email` VARCHAR(255), IN `_password` VARCHAR(255))   BEGIN
-  SELECT * FROM student WHERE email = _email && password = _password;
+  SELECT * FROM users WHERE email = _email && password = _password;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_store_announcement` (IN `_title` VARCHAR(255), IN `_description` VARCHAR(255), IN `_created_at` DATE)   BEGIN
@@ -96,7 +114,7 @@ CREATE TABLE `announcements` (
 --
 
 INSERT INTO `announcements` (`id`, `title`, `description`, `created_at`) VALUES
-(5, 'Alas 9: 15 PM na', 'wa pa koy kaon', '2023-12-07'),
+(5, 'Alas 9: 15 PM na', 'wa pa koy kaon ,mzxcn,m,m,mnxm,cn,mxnc,xmzncxz,mcnzx,mn', '2023-12-07'),
 (6, 'BOANG NGA ANNOUNCEMENT', 'TINUWA NASAD AMONG SUDAN PISTE YAWA GAITAY BAHOG LOBOGT', '2023-12-07'),
 (7, 'lorem ipsum or some shit', 'lorem ispum kokwd akwokd owaan,mdn awkoawkoa akwokaw ssadhsjhghsgs', '2023-12-07'),
 (8, 'ANNOUNCEMENT: KATUGON NAKO', 'HAHAHAHAHAAHA', '2023-12-07');
@@ -122,18 +140,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `department`, `password`, `role`) VALUES
-(1, 'Ryan', 'Gosling', 'literally me', 'BSIT', 'chaeyoung143', 'Admin'),
+(1, 'Ryan', 'Gosling', 'literally me', 'BSIT', 'mefr', 'Admin'),
 (2, 'Ice', 'Spice', 'ice@spice.e', 'BSIT', 'asdasd', 'Admin'),
 (4, 'Hev', 'Abi', 'hev@abi.com', 'BSHM', 'Admin', 'Admin'),
 (5, 'City', 'Girls', 'city@girls.com', 'BSHM', 'student', 'Student'),
 (6, 'Skibidi', 'Gyatt', 'skibidi gyatt rizz', 'BEED', '123456', 'Admin'),
 (7, 'Chae ', 'young', 'young@chae.boang', 'BSIT', 'asdasd', 'Student'),
-(12, 'asdas', 'asdasd', 'sumagangjoren@gmail.com', 'BEED', 'asdas', 'student'),
-(14, 'Joren', 'Sumagang', 'asdasda@asd', 'BEED', 'asdasd', 'student'),
 (17, 'new', 'jeans', 'oma oma godd kasu kasu wa', 'BEED', 'gyatt', 'Admin'),
-(18, 'Joren', 'Sumagang', 'sumagangjoren@gmail.com', 'BSIT', 'asdasd', 'student'),
-(19, 'Joren', 'Sumagang', 'sumagangjoren@gmail.com', 'BSIT', 'asdasd', 'student'),
-(20, 'Sumagang', 'Sumagang', 'sumagangjoren@gmail.com', 'BSIT', 'asdasd', 'Admin');
+(19, '21', 'Savagee', '21@savage.com', 'BSED', 'bruh', 'Admin'),
+(22, 'Ryan', 'Gosling', 'Ryan goslling unsint a message.', 'BSIT', 'asdasd', 'Admin'),
+(23, 'Perrell', 'Brown', 'dreamy@is.perrell', 'BSHM', 'GYATTTT', 'student'),
+(33, 'Joren', 'Sumagang', 'sumagangjoren@gmail.com', 'BSIT', 'asdasd', 'student'),
+(35, 'Admin', 'Admin', 'admin@email.com', 'BSIT', 'asdasd', 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -165,7 +183,7 @@ ALTER TABLE `announcements`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
